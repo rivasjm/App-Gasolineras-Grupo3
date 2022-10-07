3a06648b2862288af839be324d6163092ac78c9e
Marcos Fernandez Alosno <mfa909@alumnos.unican.es>
2022-10-07 15:13:49 +0200

    Añadir precios y horarios a la vista

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/detail/GasolineraDetailView.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/detail/GasolineraDetailView.java
@@ -22,16 +22,28 @@ public class GasolineraDetailView extends AppCompatActivity {
         ImageView ivRotulo = findViewById(R.id.ivRotulo);
         TextView tvRotulo = findViewById(R.id.tvRotulo);
         TextView tvMunicipio = findViewById(R.id.tvMunicipio);
+        TextView tvHorario = findViewById(R.id.tvHorario);
+        TextView tvPrecioDieselA = findViewById(R.id.tvPrecioDieselA);
+        TextView tvPrecioGasolina95 = findViewById(R.id.tvPrecioGasolina95);
+        TextView tvPrecioSumario =findViewById(R.id.tvPrecioSumario);
+
 
         // Get Gas Station from the intent that triggered this activity
         Gasolinera gasolinera = getIntent().getExtras().getParcelable(INTENT_GASOLINERA);
+        //Create item from class gasolineraDetailPresenter in order to get the added price
+        GasolineraDetailPresenter gs = new GasolineraDetailPresenter(gasolinera);
 
         // Set logo
         int imageID = getResources().getIdentifier("generic", "drawable", getPackageName());
         ivRotulo.setImageResource(imageID);
 
         // Set Texts
-        tvRotulo.setText(gasolinera.getRotulo());
-        tvMunicipio.setText(gasolinera.getMunicipio());
+        tvRotulo.setText(gs.getRotulo());
+        tvMunicipio.setText(gs.getMunicipio());
+        tvHorario.setText(gs.getSchedule());
+        tvPrecioDieselA.setText(gs.getDieselA());
+        tvPrecioGasolina95.setText(gs.getNormal95());
+        tvPrecioSumario.setText(gs.getPrecioSumario());
+
     }
 }

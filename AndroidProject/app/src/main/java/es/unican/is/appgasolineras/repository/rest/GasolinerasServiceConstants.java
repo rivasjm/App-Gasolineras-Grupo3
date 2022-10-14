a73725a290b1488561b4e670d3add09136ce9762
Alina Solonaru <aso510@alumnos.unican.es>
2022-10-14 17:21:46 +0200

    Arreglar giro horizontal toolbar
    Al girar el movil en modo horizontal, el logo se quedaba en mitad del toolbar en
    lugar de a la izquierda, esto se ha corregido.

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/repository/rest/GasolinerasServiceConstants.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/repository/rest/GasolinerasServiceConstants.java
@@ -5,6 +5,7 @@ public class GasolinerasServiceConstants {
     private static final String MINECO_API_URL =
             "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/";
 
+    // para pruebas cambiar master por develop o feature
     private static final String STATIC_API_URL =
             "https://raw.githubusercontent.com/isunican/App-Gasolineras-Grupo3/master/StaticREST/ServiciosRESTCarburantes/PrecioCarburantes/";
 

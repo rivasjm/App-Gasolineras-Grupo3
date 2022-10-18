5dee07592938c3a6da095b1b5960fad5f4b415fa
Micaela Patiño <micaela.patino@ucb.edu.bo>
2022-10-18 15:22:58 +0200

    Pruebas de interfaz

--- AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/convenios/MostrarConveniosUITest.java
+++ AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/convenios/MostrarConveniosUITest.java
@@ -1,5 +1,7 @@
 package es.unican.is.appgasolineras.activities.convenios;
+import org.hamcrest.Matchers;
 import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
@@ -9,20 +11,38 @@ import es.unican.is.appgasolineras.activities.main.MainView;
 import es.unican.is.appgasolineras.model.Convenio;
 import es.unican.is.appgasolineras.repository.rest.GasolinerasServiceConstants;
 
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static es.unican.is.appgasolineras.utils.Matchers.hasElements;
 
+import android.view.View;
+
 public class MostrarConveniosUITest {
+    private View decorView;
+    @Rule
+    public ActivityScenarioRule<ConveniosView> activityRule =
+            new ActivityScenarioRule(ConveniosView.class);
     @BeforeClass
     public static void setUp() {
         GasolinerasServiceConstants.setStaticURL();
+
+    }
+    @Before
+    public void set(){
+        activityRule.getScenario().onActivity(new ActivityScenario.ActivityAction<ConveniosView>() {
+            @Override
+            public void perform(ConveniosView activity) {
+                decorView = activity.getWindow().getDecorView();
+            }
+        });
     }
 
     @AfterClass
@@ -30,20 +50,23 @@ public class MostrarConveniosUITest {
         GasolinerasServiceConstants.setMinecoURL();
     }
 
-    @Rule
-    public ActivityScenarioRule<ConveniosView> activityRule =
-            new ActivityScenarioRule(ConveniosView.class);
+
 
     @Test
-    public void openDetailViewTest() {
-        onView(withId(R.id.lvConvenios)).check(matches(isDisplayed()));
+    public void openConveniosViewTest() {
+        onView(withText("Se han cargado 0 convenios"))
+                .inRoot(withDecorView(Matchers.not(decorView)))// Here we use decorView
+                .check(matches(isDisplayed()));
+        //onView(withId(R.id.lvConvenios)).check(matches(isDisplayed()));
+        //onView(withId(R.id.Toas))
         //onView(withId(R.id.)).check(matches(withText("CONVENIOS")));
     }
+    /*
     @Test
     public void homeIcon(){
         //onView(withId(R.id.menuHome)).perform(click());
-        onView(withId(R.id.lvGasolineras)).check(matches(isDisplayed()));
-    }
+       // onView(withId(R.id.lvGasolineras)).check(matches(isDisplayed()));
+    }*/
     public void backButton(){
 
 

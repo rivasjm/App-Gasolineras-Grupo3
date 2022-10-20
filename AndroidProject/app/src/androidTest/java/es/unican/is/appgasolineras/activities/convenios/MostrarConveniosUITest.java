ba95bc3089acea9141173882f24799b602409b84
Micaela Patiño <micaela.patino@ucb.edu.bo>
2022-10-20 11:51:46 +0200

    Pruebas de interfaz convenios
    Se implementaron las pruebas de interfaz para los convenios tomando en cuenta los escenarios en los que estan vacios

--- AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/convenios/MostrarConveniosUITest.java
+++ AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/convenios/MostrarConveniosUITest.java
@@ -1,64 +1,61 @@
 package es.unican.is.appgasolineras.activities.convenios;
-import org.hamcrest.Matchers;
+
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import es.unican.is.appgasolineras.R;
 import es.unican.is.appgasolineras.activities.main.MainView;
-import es.unican.is.appgasolineras.model.Convenio;
 import es.unican.is.appgasolineras.repository.rest.GasolinerasServiceConstants;
 
-import androidx.test.core.app.ActivityScenario;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.withDecorView;
-import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.any;
-import static org.hamcrest.Matchers.not;
-import static es.unican.is.appgasolineras.utils.Matchers.hasElements;
 
-import android.view.View;
+import static org.hamcrest.Matchers.anyOf;
 
+@RunWith(AndroidJUnit4.class)
 public class MostrarConveniosUITest {
-    private View decorView;
     @Rule
-    public ActivityScenarioRule<ConveniosView> activityRule =
-            new ActivityScenarioRule(ConveniosView.class);
+    public ActivityScenarioRule<MainView> activityRule =
+            new ActivityScenarioRule(MainView.class);
+
     @BeforeClass
     public static void setUp() {
         GasolinerasServiceConstants.setStaticURL();
     }
-    /*
-    @Before
-    public void set(){
-        activityRule.getScenario().onActivity(new ActivityScenario.ActivityAction<ConveniosView>() {
-            @Override
-            public void perform(ConveniosView activity) {
-                decorView = activity.getWindow().getDecorView();
-            }
-        });
-    }*/
 
     @AfterClass
     public static void clean() {
         GasolinerasServiceConstants.setMinecoURL();
     }
+
     @Test
-    public void openEmptyConveniosViewTest() {
+    public void testConvenioVacioVolverAtras() {
+        openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        onView(anyOf(withText("Convenios"), withId(R.id.menuConvenios))).perform(click());
         onView(withId(R.id.tvConveniosVacio)).check(matches(withText("La lista de convenios esta vacía")));
-        onView(withId(R.id.lvConvenios)).check(matches(not(hasDescendant(any(View.class)))));
+        pressBack();
+        onView(withId(R.id.lvGasolineras)).check(matches(isDisplayed()));
     }
 
-
-
-
+    @Test
+    public void testConvenioVacioVolverBotonInicio() {
+        openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        onView(anyOf(withText("Convenios"), withId(R.id.menuConvenios))).perform(click());
+        onView(withId(R.id.tvConveniosVacio)).check(matches(withText("La lista de convenios esta vacía")));
+        onView(withId(R.id.ivLogo)).perform(click());
+        onView(withId(R.id.lvGasolineras)).check(matches(isDisplayed()));
+    }
 }

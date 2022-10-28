603c9d8a2386b67ce8bea0fae4e78b69068a22b8
Iván Ortiz del Noval <ivan.ortizd@alumnos.unican.es>
2022-10-28 18:00:08 +0200

    Solucionar error integración continua

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/toolbar/BarraHerramientasPresenter.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/toolbar/BarraHerramientasPresenter.java
@@ -62,20 +62,4 @@ public class BarraHerramientasPresenter implements IBarraHerramientasContract.Pr
             }
         }
     }
-
-    @Override
-    public void onOrdenarDistanciaClicked() {
-        view.showOrdenarDistanciaSelected();
-        view.showOrdenarPrecioDeselected();
-        // Creo que aqui iria el codigo de la funcionalidad
-
-    }
-
-    @Override
-    public void onOrdenarPrecioClicked() {
-        view.showOrdenarPrecioSelected();
-        view.showOrdenarDistanciaDeselected();
-        // Creo que aqui iria el codigo de la funcionalidad
-
-    }
 }

6fde7667f43c9bf23775df60945c4dbc6fa04f92
Micaela Patiño <micaela.patino@ucb.edu.bo>
2022-11-02 18:00:41 +0100

    Finalizar detalles de la funcionalidad y la arquitectura
    Se completan detalles de la funcionalidad y se actualiza el diagrama de la arquitectura

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/common/GasolineraPrecioComparator.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/common/GasolineraPrecioComparator.java
@@ -10,9 +10,9 @@ public class GasolineraPrecioComparator implements Comparator<Gasolinera> {
      * Compara gasolineras por su precio sumario.
      * @param g1 Una
      * @param g2 Otra
-     * @return -1 si Una tiene un precio sumario inferior que el de la otra gasolinera
-     *          0 si el precio sumario es el mismo
-     *          1 si Una tiene un precio sumario superior que el de la otra gasolinera
+     * @return -1 si g1 tiene un precio sumario inferior que el de g2
+     *          0 si el precio sumario de ambas gasolineras es el mismo
+     *          1 si g1 tiene un precio sumario superior que el de g2
      */
     @Override
     public int compare(Gasolinera g1, Gasolinera g2) {

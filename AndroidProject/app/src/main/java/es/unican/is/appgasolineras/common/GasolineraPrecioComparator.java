f9276f7715db5ad5815fa27ed844bb277f089df4
Micaela Patiño <micaela.patino@ucb.edu.bo>
2022-11-02 16:29:22 +0100

    Arreglar comparator

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/common/GasolineraPrecioComparator.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/common/GasolineraPrecioComparator.java
@@ -8,11 +8,11 @@ import es.unican.is.appgasolineras.model.Gasolinera;
 public class GasolineraPrecioComparator implements Comparator<Gasolinera> {
     /**
      * Compara gasolineras por su precio sumario.
-     * @param gasolinera Una
-     * @param otra Otra
-     * @return -1 si Una esta mas cerca que la otra de la ubicacion base
-     *          0 si estan a la misma distancia
-     *          1 si otra esta mas cerca que una de la ubicacion base
+     * @param g1 Una
+     * @param g2 Otra
+     * @return -1 si Una tiene un precio sumario inferior que el de la otra gasolinera
+     *          0 si el precio sumario es el mismo
+     *          1 si Una tiene un precio sumario superior que el de la otra gasolinera
      */
     @Override
     public int compare(Gasolinera g1, Gasolinera g2) {

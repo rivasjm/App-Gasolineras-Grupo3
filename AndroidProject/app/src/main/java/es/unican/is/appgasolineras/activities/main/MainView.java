7e76df323da85e8fcc71621566cc03ca14150ccc
Marcos Fernandez <mfa909@alumnos.unican.es>
2022-11-11 14:24:43 +0100

    Mejorar refresh

--- AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/main/MainView.java
+++ AndroidProject/app/src/main/java/es/unican/is/appgasolineras/activities/main/MainView.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.swiperefreshlayout.*;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 
